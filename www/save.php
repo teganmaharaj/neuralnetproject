03ad25cde9e164803894e160d25a02511f7694a0
Jordan Slaman <jordan.slaman@gmail.com>
2013-02-01 19:55:04 -0500

    Working on the form a bit
    Will need to set up myserver in order to do further testing

--- www/save.php
+++ www/save.php
@@ -4,12 +4,32 @@ require_once 'genImage.php';
 
 $img = sigJsonToImage(file_get_contents($_POST['output']));
 
-// Save to file
-imagepng($img, 'image.png');
+$date = new DateTime();
+$filename = $date->format('U = Y-m-d-H-i-s')+'x'+$_POST['value'];
 
 // Output to browser
 header('Content-Type: image/png');
 imagepng($img);
+
+// Save to file
+imagepng($img, $filename+'.png');
+
+for ($y=0; $y < 150; $y++) {
+	for ($x=0; $x < 100; $x++) {
+			if (imagecolorat($img, $x, $y) == 0){
+				print '1';
+			} else {
+				print '0';
+			}
+	}
+	print "\n";
+}
+
+$string = '010010101001010010010010111010101101001010100100100101011101011010010101010100101010010101001011101001';
+
+//exec('neuralnet'+$_POST['value']+$string);
+
+
 
 // Destroy the image in memory when complete
 imagedestroy($img);
