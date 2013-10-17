5b6b79652a73ff5db5cf72f5223e243c0d64e9d6
geoff <geoff@geoff-K55A.(none)>
2013-10-17 15:56:12 -0400

    Added System 	new file:   System.h 	modified:   System.h

---
+++ cpp/Neurons/System.h
@@ -0,0 +1,12 @@
+#ifndef __SYSTEM__
+#define __SYSTEM__
+
+class System
+{
+  public:
+  static bool isRunning;
+  int layerOn;
+  void on(){isRunning = true;}
+  void off(){isRunning = false;}
+};
+#endif
